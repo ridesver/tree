b8c16701a9ea390908a12630e536938b402f2d9d
Daria Eremitskaya <31545001+ridesver@users.noreply.github.com>
2018-05-10 16:15:53 +0300

    Update and rename vector.hpp to example.cpp

---
+++ examples/example.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "tree_t.hpp"
+
+using namespace std;
+
+int main() {
+	tree_t<int> tree;
+	char op;
+	while (cin >> op && op != 'q') {
+		int num;
+		switch (op) {
+		case '=':
+			tree.print(cout);
+			break;
+		case '+':
+			if (cin >> num) {
+				tree.insert(num);
+				tree.print(cout);
+			}
+			else {
+				cout << "An error has occured while reading input data\n";
+				exit(0);
+			}
+			break;
+		case '?':
+			if (cin >> num) {
+				if (tree.find(num)) {
+					cout << "true\n";
+				}
+				else {
+					cout << "false\n";
+				}
+			}
+			else {
+				cout << "An error has occured while reading input data\n";
+				exit(0);
+			}
+			break;
+		}
+	}
+	return 0;
+}
